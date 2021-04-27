8e270469c1f92dae962bd2380a050e3d24b4d204
leonardo santos <72804534+leonardosantos07@users.noreply.github.com>
2021-04-27 20:31:29 -0300

    teste - novo repositório

--- estrutura_decisao/ex20.py
+++ estrutura_decisao/ex20.py
@@ -4,7 +4,6 @@
 #A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
 #A mensagem "Aprovado com Distinção", se a média for igual a 10.
 
-
 nota1 = float(input('informe a primeira nota:'))
 nota2 = float(input('informe a segunda nota:'))
 nota3 = float(input('informe a terceira nota:'))
